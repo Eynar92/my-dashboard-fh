29ef7709348df5e0a836e95ed2c538e11fe53eee
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-12-26 01:03:28 -0400

    Implement API Fetch, General View with Cards, and SSR in Next.js.

---
+++ src/pokemons/components/PokemonGrid.tsx
@@ -0,0 +1,21 @@
+// import { PokemonCard, SimplePokemon } from "..";
+
+import { PokemonCard, SimplePokemon } from "..";
+
+interface Props {
+    pokemons: SimplePokemon[];
+}
+
+export const PokemonGrid = ({ pokemons }: Props) => {
+    return (
+        <div className="flex flex-wrap gap-10 items-center justify-center">
+            {
+                pokemons.map(pokemon => (
+
+                    <PokemonCard pokemon={pokemon} key={pokemon.id} />
+
+                ))
+            }
+        </div>
+    )
+}
